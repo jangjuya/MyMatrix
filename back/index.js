ed977e6f1c8f8d1ad44a568db1d6e40860e1ef6f
jangjuya <kyyy1230@naver.com>
2023-02-06 21:51:45 +0900

    first commit

---
+++ back/index.js
@@ -0,0 +1,21 @@
+const compression = require('compression');
+const cors = require("cors");
+const { indexRouter } = require("./src/router/indexRouter");
+const { userRouter } = require("./src/router/userRouter");
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(compression());
+
+indexRouter(app);
+userRouter(app);
+
+app.listen(port, () => {
+    console.log(`Express app listening at port: ${port}`)
+})
